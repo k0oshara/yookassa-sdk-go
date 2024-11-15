ca1c18b02ffdac1c665c95cf71f5bd9454872b19
Maksim <mi9992828@gmail.com>
2024-11-16 02:35:33 +0300

    fork rep rvinnie

--- test/unit/payment/card_test.go
+++ test/unit/payment/card_test.go
@@ -2,7 +2,7 @@ package payment
 
 import (
 	"encoding/json"
-	yoopayment "github.com/rvinnie/yookassa-sdk-go/yookassa/payment"
+	yoopayment "github.com/k0oshara/yookassa-sdk-go/yookassa/payment"
 	"reflect"
 	"testing"
 )
